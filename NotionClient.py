5570a3163dd17a19f0824604d9384d51ed0fb564
Davina Zaman <davzaman@gmail.com>
2023-01-06 12:41:02 -0800

    update client to deal with content that's too nested

--- NotionClient.py
+++ NotionClient.py
@@ -1,4 +1,5 @@
-from requests import Session
+from requests import Session, Response
+from requests.exceptions import HTTPError
 from typing import List, Dict, Optional, Tuple
 
 # Just bookkeeping for reference
@@ -51,6 +52,9 @@ class NotionClient:
             "Content-Type": "application/json",
         }
 
+    #############################
+    #  non-mutating operations  #
+    #############################
     def get_page_obj_json(
         self, session: Session, page_name: Optional[str] = None, is_db: bool = False
     ) -> Dict:
@@ -154,6 +158,41 @@ class NotionClient:
 
         return elements
 
+    #########################
+    #  mutating operations  #
+    #########################
+    def add_nested_content(
+        self, session: Session, grandparent_id: str, nested_content: List[Dict]
+    ):
+        """
+        APPEND CHILDREN endpoint.
+        https://developers.notion.com/reference/patch-block-children
+        API Currently limited nested blocks to 2 levels.
+        This function allows us to split the content and add each part separately to work around this limitation.
+        Keeps splitting parent/children until nesting limitation isn't violated.
+        Works even when we don't know how deep it's nested.
+        """
+        parents, children = self._split_parent_children(nested_content)
+
+        res_parents = session.patch(
+            f"{self.API_URL}/blocks/{grandparent_id}/children",
+            headers=self.headers,
+            json={"children": parents},
+        )
+        res_parents.raise_for_status()
+
+        # for each header obj add its subcontent we stripped earlier
+        parent_ids = [header_obj["id"] for header_obj in res_parents.json()["results"]]
+        for parent_id, child_content in zip(parent_ids, children):
+            # Add child content to the parent
+            res_child = session.patch(
+                f"{self.API_URL}/blocks/{parent_id}/children",
+                headers=self.headers,
+                json={"children": child_content},
+            )
+            if self._content_too_nested(res_child):  # recursive call
+                self.add_nested_content(session, parent_id, child_content)
+
     def create_new_subpage(
         self,
         session: Session,
@@ -173,7 +212,19 @@ class NotionClient:
             payload["children"] = page_content
 
         res = session.post(f"{self.API_URL}/pages", headers=self.headers, json=payload)
-        res.raise_for_status()
+
+        if self._content_too_nested(res):  # content is too nested.
+            del payload["children"]  # no children
+            # Create empty page
+            res_empty_pg = session.post(
+                f"{self.API_URL}/pages", headers=self.headers, json=payload
+            )
+            res_empty_pg.raise_for_status()
+
+            # Add content to the empty page aware of nesting limitations
+            empty_pg_id = res_empty_pg.json()["id"]
+            self.add_nested_content(session, empty_pg_id, page_content)
+
         return res.text
 
     def delete_block(self, session: Session, block_id: str) -> str:
@@ -184,3 +235,35 @@ class NotionClient:
         res = session.delete(f"{self.API_URL}/blocks/{block_id}", headers=self.headers)
         res.raise_for_status()
         return res.text
+
+    #############
+    #  HELPERS  #
+    #############
+    @staticmethod
+    def _split_parent_children(
+        content: List[Dict],
+    ) -> Tuple[List[Dict], List[List[Dict]]]:
+        """
+        Splits block objects into parents and children.
+        Example:
+        [ {b1 ... children: [{c1}] }, {b2 ... children: [{c2_1, c2_2}]} ]
+        ==>
+        parents: [{b1 ... }, {b2 ...}]
+        children: [[{c1}], [{c2_1, c2_2}]]
+        """
+        children = []
+        for block in content:  # strips + tracks the children of each parent
+            children.append(block[block["type"]].pop("children"))
+        return (content, children)
+
+    @staticmethod
+    def _content_too_nested(response: Response) -> bool:
+        try:
+            response.raise_for_status()
+        except HTTPError:
+            error = response.json()
+            return (
+                error["code"] == "validation_error"
+                and "chlidren should be not present" in error["message"]
+            )
+        return False  # Otherwise no error, so we're good.
